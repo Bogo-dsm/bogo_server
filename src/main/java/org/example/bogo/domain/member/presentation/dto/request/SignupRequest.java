3abc9ce9eee7f7eab2e58d2984c7b108249ae35a
gh7035 <gh7035@daum.net>
2026-03-05 16:17:42 +0900

    chore: 비밀번호 형식 미지정 부분 지정(#3)

--- src/main/java/org/example/bogo/domain/member/presentation/dto/request/SignupRequest.java
+++ src/main/java/org/example/bogo/domain/member/presentation/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package org.example.bogo.domain.member.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SignupRequest(
@@ -14,6 +15,10 @@ public record SignupRequest(
         String email,
 
         @NotBlank
-        @Size(min = 4, max = 255)
+        @Size(min = 8, max = 255)
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
+                message = "Password must include letters and numbers"
+        )
         String password
 ) {}
